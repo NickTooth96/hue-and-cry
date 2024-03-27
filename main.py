4cc4dacf1c8c39d5532244ee91e6f3cb5eb67c15
Nick Toothaker <nicktoothaker@gmail.com>
2024-03-27 18:54:17 -0500

    ADDED threading

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ import src.discover as discover
 parser = argparse.ArgumentParser(description='Search for files')
 parser.add_argument('--key', type=str, help='Search term')
 parser.add_argument('--path', type=str, help='Path to search', default=os.path.expanduser('~'))
+parser.add_argument('--dev', action='store_true', help='Search recursively')
+parser.add_argument('--search', action='store_true', help='Search recursively')
 
 args = parser.parse_args()
 
-print(args)
 
 
-run_start = datetime.datetime.now()
+if args.search:
 
-out = discover.hue(args.key, args.path)
+    run_start = datetime.datetime.now()
 
-i = 1
-for f in out:
-    print(f'{i}:',f,out[f])
-    i += 1
-print("Time taken:",datetime.datetime.now()-run_start)
+    out = discover.hue(args.key, args.path)
+
+    i = 1
+    for f in out:
+        print(f'{i}:',f,out[f])
+        i += 1
+    print("Time taken:",datetime.datetime.now()-run_start)
+
+if args.dev:
+    listed = discover.threads(key=args.key, search_path=args.path)
+    for k,v in listed.items():
+        print(f"{k}: {v}")
+
+
--- src/discover.py
+++ src/discover.py
@@ -1,21 +1,36 @@
 import os 
+import threading
 import time
 
 
-def get_list(src=os.getcwd()):
+def get_list_as_dictionary_recursive(src=os.getcwd()):
     """returns list of all files in directory
 
     Args:
         src (_type_): _description_
 
     Returns:
-        list: list of all files in current directory (dest)
-    """
-    buffer = []
-    output = []
+        dict: dictionary of all files in current directory (src)
+    """    
     if src != os.getcwd():
         src = os.path.join('',src)
-    return os.listdir(src)
+    out =  {i:os.path.join(src,i) for i in os.listdir(src)}
+    return {k: get_list_as_dictionary_recursive(v) if os.path.isdir(v) else v for k,v in out.items()} 
+
+def get_list_as_dictionary(src=os.getcwd()):
+    """returns list of all files in directory
+
+    Args:
+        src (_type_): _description_
+
+    Returns:
+        dict: dictionary of all files in current directory (src)
+    """    
+    if src != os.getcwd():
+        src = os.path.join('',src)
+    out =  {i:os.path.join(src,i) for i in os.listdir(src)}
+    ## change  so that the value is True if it is a directory and the key as the full path
+    return {v: True if os.path.isdir(v) else v for k,v in out.items()}
 
 def recursive_list(src=os.getcwd()):
     """returns list of all files in directory and subdirectories
@@ -42,7 +57,7 @@ def hue(key,search_path=os.path.expanduser('~')):
     print(f'--- Execution Time: {run_time:.2f} ---')  
     return results
 
-def cry(key,search_path):
+def _cry(key,search_path):
 
     results = {}
 
@@ -54,7 +69,48 @@ def cry(key,search_path):
                     results[file] = os.path.join(subdir, file)
     return results
 
+def cry(key, element):
+    if key in element:
+        return True
+    else:
+        return False
+
 def clickable_link(url, text=None):
     if text is None:
         text = url
     return f"\033]8;;{url}\033\\{text}\033]8;;\033\\"
+
+def multi_hue(key, search_path):
+    dirlist = get_list_as_dictionary(search_path)
+    print(dirlist)
+
+def threads(key, search_path, thread_num=1, output={}):
+    dir_list = os.listdir(search_path)
+    for element in dir_list:
+        element = os.path.join(search_path, element)
+       
+        if os.path.isdir(element):
+            print(f"Thread {thread_num} is searching {element} for {key}")
+            t = threading.Thread(target=threads, args=(key,element, thread_num+1, output))
+            t.start()
+            t.join()
+        else:
+            if cry(key, element):
+                print(f"Found {key} in {search_path}")
+                score = string_hamming_distance(key, element.split('/')[-1])
+                output[score] = os.path.join(search_path, element) 
+            else:
+                pass
+    output = dict(sorted(output.items()))
+    return output
+
+def string_hamming_distance(s1, s2):
+    s1 = s1.strip().lower()
+    s2 = s2.strip().lower()
+    distance = 0
+    if len(s1) != len(s2):
+        distance += abs(len(s1) - len(s2))
+    for i in range(min(len(s1), len(s2))):
+        if s1[i] != s2[i]:
+            distance += 1
+    return distance
